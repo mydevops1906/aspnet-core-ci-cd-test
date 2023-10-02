69f33f853bff27a24fc0d3b8b248ce084315013c
mydevops1906 <146276276+mydevops1906@users.noreply.github.com>
2023-10-02 21:54:32 +0000

    added docker, dockercompose and start.sh for ci/cd stuff

---
+++ Controllers/TestController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_core_ci_cd_test.Controllers;
+
+[ApiController]
+[Route("/test")]
+public class TestController : ControllerBase
+{
+    
+
+    [HttpGet]
+    public string Get()
+    {
+       return "techbuddy";
+    }
+}
